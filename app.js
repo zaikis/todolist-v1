82829437a549735b0de227c27c9e2e4d04169d8a
chris zaikis <62755319+zaikis@users.noreply.github.com>
2022-03-24 13:12:27 +0200

    todolist-v1

---
+++ app.js
@@ -0,0 +1,47 @@
+//jshint esversion:6
+
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+
+const app = express();
+
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const WorkItems = [];
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.get("/", function(req, res) {
+
+const day = date.getDate();
+
+
+  res.render("list", {listTitle: day, newListItems: items});
+
+});
+
+app.post("/", function(req,res){
+
+  const item = req.body.newItem;
+
+  if (req.body.list === "work") {
+    WorkItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+
+});
+
+ app.get("/work",function(req,res){
+   res.render("list", {listTitle: "Work list" , newListItems: WorkItems});
+ });
+
+
+app.listen(3000, function() {
+  console.log("Server is running on port 3000");
+});
